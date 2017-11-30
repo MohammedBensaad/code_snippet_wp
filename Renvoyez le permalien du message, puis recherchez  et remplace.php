cca89f97f29a6a7e1283cdcbaee13d46d88f70fe
MohammedBensaad <dev.itga@gmail.com>
2017-11-30 09:44:28 +0100

    permalien recherche et remplace
    Renvoyez le permalien du message, puis recherchez http://www.monsite.com/ et remplacez-le par http://www.yoursite.com/

---
+++ Renvoyez le permalien du message, puis recherchez  et remplace.php
@@ -0,0 +1,4 @@
+Renvoyez le permalien du message, puis recherchez http://www.monsite.com/ et remplacez-le par http://www.yoursite.com/
+
+
+<?php echo str_replace("http://www.mysite.com/","http://www.yoursite.com/",get_permalink()); ?>
